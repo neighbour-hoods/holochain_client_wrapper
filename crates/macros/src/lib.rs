c57ab9f7bc7226938aab28fddb914abb86dd92ec
Michael Hueschen <m@mhueschen.space>
2022-08-11 12:56:12 -0600

    stub out `generate_call` proc macro
    consolidate both ena/disableApp to use `call` (which crashes w/
    `todo!()` right now).

--- crates/macros/src/lib.rs
+++ crates/macros/src/lib.rs
@@ -2,3 +2,13 @@
 
 use proc_macro::TokenStream;
 use proc_macro2::{Ident, Span};
+
+#[proc_macro]
+pub fn generate_call(input: TokenStream) -> TokenStream {
+    (quote::quote! {
+        pub async fn call(&self, cmd: AdminWsCmd) -> Result<JsValue, JsValue> {
+            todo!()
+        }
+    })
+    .into()
+}
