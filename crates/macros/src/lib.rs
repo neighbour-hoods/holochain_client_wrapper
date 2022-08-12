e1af5988373a44eff76d9defdf251154b56c7a0a
Michael Hueschen <m@mhueschen.space>
2022-08-12 08:15:14 -0600

    macros: structural refactor to rework unit-case
    we need to construct `payload` differently to make things work, so I had
    to expose / rework things.

--- crates/macros/src/lib.rs
+++ crates/macros/src/lib.rs
@@ -14,57 +14,65 @@ pub fn generate_call(_attrs: TokenStream, item: TokenStream) -> TokenStream {
         let variant_name = variant.ident.clone();
         let variant_name_camel_case = lowercase_first_letter(variant.ident.to_string());
 
-        let variant_fields_ident_vec: Vec<Ident> = match &variant.fields {
-            Fields::Unnamed(_) => panic!("unnamed fields are not allowed"),
-            // TODO we need to handle the unit case differently - not as a map, just a unit type
-            Fields::Unit => vec![],
-            Fields::Named(fields_named) => fields_named
-                .named
-                .iter()
-                .map(|field| field.ident.clone().expect("field should have ident"))
-                .collect(),
-        };
-        let variant_fields_ident_comma_punctuated: Punctuated<Ident, Comma> = {
-            let mut ret = Punctuated::new();
-            for ident in variant_fields_ident_vec.iter().cloned() {
-                ret.push(ident);
-            }
-            ret
-        };
+        let (payload_construction_tokenstream, enum_match_binder): (TokenStream2, TokenStream2) =
+            match &variant.fields {
+                Fields::Unnamed(_) => panic!("unnamed fields are not allowed"),
+                // TODO we need to handle the unit case differently - not as a map, just a unit type
+                Fields::Unit => {
+                    todo!()
+                }
+                Fields::Named(fields_named) => {
+                    let variant_fields_ident_vec: Vec<Ident> = fields_named
+                        .named
+                        .iter()
+                        .map(|field| field.ident.clone().expect("field should have ident"))
+                        .collect();
+                    let variant_fields_ident_comma_punctuated: Punctuated<Ident, Comma> =
+                        Punctuated::from_iter(variant_fields_ident_vec.iter().cloned());
 
-        let field_insertion_blob: TokenStream2 = {
-            let mut field_insertion_blob = TokenStream2::new();
-            for field_ident in variant_fields_ident_vec {
-                field_insertion_blob.extend(quote::quote_spanned! {variant.span() =>
-                    assert!(Reflect::set(
-                        &payload,
-                        &(stringify!(#field_ident).into()),
-                        // TODO here we might consider adding a Trait to handle custom
-                        // converting-to-object for types we can then offer as strongly-typed
-                        // within the `AdminWsCmd` enum.
-                        //
-                        // `.into()` will only work for simple Rust types...
-                        &(#field_ident.into()),
-                    )?);
-                });
-            }
-            field_insertion_blob
-        };
+                    let field_insertion_blob: TokenStream2 = {
+                        let mut field_insertion_blob = TokenStream2::new();
+                        for field_ident in variant_fields_ident_vec {
+                            field_insertion_blob.extend(quote::quote_spanned! {variant.span() =>
+                                assert!(Reflect::set(
+                                    &payload,
+                                    &(stringify!(#field_ident).into()),
+                                    // TODO here we might consider adding a Trait to handle custom
+                                    // converting-to-object for types we can then offer as strongly-typed
+                                    // within the `AdminWsCmd` enum.
+                                    //
+                                    // `.into()` will only work for simple Rust types...
+                                    &(#field_ident.into()),
+                                )?);
+                            });
+                        }
+                        field_insertion_blob
+                    };
+
+                    let payload_construction_tokenstream: TokenStream2 = quote::quote! {
+                        let payload: JsValue = Object::new().dyn_into()?;
+                        assert!(Reflect::set(
+                            &payload,
+                            &JsValue::from_str("tag"),
+                            &JsValue::from_str(tag)
+                        )?);
+                        #field_insertion_blob
+                        payload
+                    };
+
+                    let enum_match_binder: TokenStream2 = quote::quote! {
+                        #variant_name { #variant_fields_ident_comma_punctuated }
+                    };
+
+                    (payload_construction_tokenstream, enum_match_binder)
+                }
+            };
 
         match_blocks.extend(quote::quote_spanned! {variant.span()=>
-            #enum_name::#variant_name { #variant_fields_ident_comma_punctuated } => {
+            #enum_name::#enum_match_binder => {
                 let tag: &str = #variant_name_camel_case;
                 let method: Function = Reflect::get(&self.js_ws, &JsValue::from_str(&tag))?.dyn_into()?;
-                let payload: JsValue = {
-                    let payload: JsValue = Object::new().dyn_into()?;
-                    assert!(Reflect::set(
-                        &payload,
-                        &JsValue::from_str("tag"),
-                        &JsValue::from_str(tag)
-                    )?);
-                    #field_insertion_blob
-                    payload
-                };
+                #payload_construction_tokenstream
                 let promise: Promise = method.call1(&self.js_ws, &payload)?.dyn_into()?;
                 let future: JsFuture = promise.into();
                 future.await
