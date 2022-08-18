817c5a0aac3e427dff01ce5b991c225de9353aa5
Michael Hueschen <m@mhueschen.space>
2022-08-18 08:37:54 -0600

    remove stale esbuild config

--- holochain_client_wrapper/esbuild.config.js
+++
@@ -1,7 +0,0 @@
-require('esbuild').build({
-  entryPoints: ['../submodules/holochain-client-js/src/index.ts'],
-  bundle: true,
-  outfile: 'crates/holochain_client_wrapper/src/holochain_client_wrapper.js',
-  format: 'esm',
-}).then(_ => console.log("success 🚀"))
-  .catch(() => process.exit(1))
